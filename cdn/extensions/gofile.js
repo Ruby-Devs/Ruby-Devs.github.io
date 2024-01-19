9c54e44b398f71a060241415647ba25dfd268086
Cicero <150862765+cicerorph@users.noreply.github.com>
2024-01-19 11:36:47 -0300

    Update gofile.js

--- cdn/extensions/gofile.js
+++ cdn/extensions/gofile.js
@@ -64,7 +64,7 @@ class GofileUploader {
             .then(response => response.json())
             .then(data => {
               if (data.status === 'ok') {
-                const directDownloadLink = `${data.data.downloadPage}`;
+                const directDownloadLink = `https://${bestServer}.gofile.io/download/${data.data.parentFolder}/${filename}`;
                 resolve(directDownloadLink);
               } else {
                 resolve(null);
