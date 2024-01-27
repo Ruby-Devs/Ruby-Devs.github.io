52efe479b2e5fd8210b28f81037d589c220d0411
meysam-iran <148990039+meysam-iran@users.noreply.github.com>
2024-01-27 18:03:21 +0330

    add 2 new surahs

--- cdn/extensions/quran-ext.js
+++ cdn/extensions/quran-ext.js
@@ -7,7 +7,9 @@
   var list = {
     '1': 'god is great, i seek refuge with allah from the accursed shaytan, in the name of allah, the most merciful, all praise is due to allah, lord of the worlds, the most merciful, sovereign of the day of recompense, it is you we worship and you we ask for help, guide us to the right path, the path of those upon whom you have bestowed favor, not those who have evoked your anger or of those who are astray'
     '2': 'alif lam meem, this is the book about which there is no doubt and a guidance for those conscios of allah, who believe in the unseen establish prayer and spend out of what we have provided for them, and who believe in what has been revealed to you and what was revealed before you and of the hereafter they are certain'
-    '3': 'alif lam meen, allah - there is no deity except him the ever-living the sustainer of existence, he has sent down upon you the book in truth confirming what was before it and he revealed the torah an d the gospel'
+    '3': 'alif lam meen, allah - there is no deity except him the ever-living the sustainer of existence, he has sent down upon you the book in truth confirming what was before it and he revealed the torah and the gospel'
+    '4': 'O mankind, fear your lord, who made you from 1 soul and made from it its mate and dispersed from both of them many men and women and fear allah through whom you ask 1 another and the wombs indeed allah is ever over you a observer, and give to the orphans their properties and dont substitute the defective for the good of theirs and dont consume their properties into your own indeed that is a sin'
+    '5': 'O you who have believed, fulfill contracts lawful for you are the animals of grazing livestock except for that which is recited to you - hunting not being permitted while you are in the state of ihram indeed allah ordains what he intends'
 }
   class Extension {
     getInfo() {
@@ -36,7 +38,7 @@ blocks.push({
 Extension.prototype["find-sureh"] = function(args, util) {
   const localVars = {};
     var t = JSON.parse('{list);
-    alert(t[[input]]) ;
+    return t[["input"]];
 };
 
 
