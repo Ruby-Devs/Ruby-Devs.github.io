90f4d11b95283c063320cad3ab079072217e540b
Cicero <150862765+cicerorph@users.noreply.github.com>
2024-03-03 17:47:22 -0300

    Update penguingpt.js

--- cdn/extensions/penguingpt.js
+++ cdn/extensions/penguingpt.js
@@ -237,7 +237,7 @@
                     },
                     {
                         opcode: 'generateImageAndImport',
-                        blockType: Scratch.BlockType.REPORTER,
+                        blockType: Scratch.BlockType.COMMAND,
                         text: 'Generate [PROMPT] from [MODEL] and import as costume with name [NAME]',
                         arguments: {
                             PROMPT: {
